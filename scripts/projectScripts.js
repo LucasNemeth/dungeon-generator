ef468be3f560071ff3cd4012f758b50726b16ad6
LucasNemeth <l_orion_n@hotmail.com>
2019-10-05 17:54:51 -0400

    got colours on our floors

--- scripts/projectScripts.js
+++ scripts/projectScripts.js
@@ -1,7 +1,29 @@
 //Doc ready
 $(function(){
 //We're going to be doing a dungeon generator for tabletop games. Something simple to essentially have the layout.
-
+    const changeFloors = () => {
+        $('button.floorGrass').click(function(){
+            $('#floor').removeClass();
+            $('#floor').addClass('floorGrass');
+        });
+        $('button.floorRocks').click(function(){
+            $('#floor').removeClass();
+            $('#floor').addClass('floorRocks');
+        });
+        $('button.floorWood').click(function(){
+            $('#floor').removeClass();
+            $('#floor').addClass('floorWood');
+        });
+        $('button.floorWater').click(function(){
+            $('#floor').removeClass();
+            $('#floor').addClass('floorWater');
+        });
+        $('button.floorLava').click(function(){
+            $('#floor').removeClass();
+            $('#floor').addClass('floorLava');
+        });
+    }
+    changeFloors();
 
 
 });
