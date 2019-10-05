78ae6cf46f6c8be9a44cfc9b6929bb309be5b82c
LucasNemeth <l_orion_n@hotmail.com>
2019-10-05 19:28:04 -0400

    Got the Obstacles taken care of

--- scripts/projectScripts.js
+++ scripts/projectScripts.js
@@ -5,25 +5,35 @@ $(function(){
         $('button.floorGrass').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorGrass');
+            $('.obstacleOptions div').hide();
+            $('.obstacleOptionsGrass').show();
         });
-        $('button.floorRocks').click(function(){
+        $('button.floorStone').click(function(){
             $('#floor').removeClass();
-            $('#floor').addClass('floorRocks');
+            $('#floor').addClass('floorStone');
+            $('#obstacleOptions div').hide();
         });
         $('button.floorWood').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorWood');
+            $('#obstacleOptions div').hide();
         });
         $('button.floorWater').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorWater');
+            $('#obstacleOptions div').hide();
         });
         $('button.floorLava').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorLava');
+            $('#obstacleOptions div').hide();
         });
+
     }
     changeFloors();
 
-
+// if #floor has class a specific class, have conditional show/hide on options. 
+// ie. if #floor has a class of .floorWater, show options for water obstacles, like Sharks, debris, boats, pirate enemies etc.
+//have them display=none by default, but when that class is selected, have em display=block
 });
+//Sorry for the soaking wet code here//
