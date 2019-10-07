3631ac38ff29c8c176198cd788eef19b9d429ab4
LucasNemeth <l_orion_n@hotmail.com>
2019-10-07 10:01:19 -0400

    Got a lot of stuff done, got the obstacles done

--- scripts/projectScripts.js
+++ scripts/projectScripts.js
@@ -11,29 +11,61 @@ $(function(){
         $('button.floorStone').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorStone');
-            $('#obstacleOptions div').hide();
+            $('.obstacleOptions div').hide();
+            $('.obstacleOptionsStone').show();
         });
         $('button.floorWood').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorWood');
-            $('#obstacleOptions div').hide();
+            $('.obstacleOptions div').hide();
+            $('.obstacleOptionsWood').show();
         });
         $('button.floorWater').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorWater');
-            $('#obstacleOptions div').hide();
+            $('.obstacleOptions div').hide();
+            $('.obstacleOptionsWater').show();
         });
         $('button.floorLava').click(function(){
             $('#floor').removeClass();
             $('#floor').addClass('floorLava');
-            $('#obstacleOptions div').hide();
+            $('.obstacleOptions div').hide();
+            $('.obstacleOptionsLava').show();
         });
 
-    }
+    };
     changeFloors();
-
+    //add the obstacles//
+    const addObstacles = () => {
+        $('button.bushObstacle').click(function(){
+            $('#obstacles').removeClass();
+            $('#obstacles').addClass('bushObstacle');
+        });
+        $('button.treeObstacle').click(function(){
+            $('#obstacles').removeClass();
+            $('#obstacles').addClass('treeObstacle');
+        });
+        $('button.pitObstacle').click(function(){
+            $('#obstacles').removeClass();
+            $('#obstacles').addClass('pitObstacle');
+        });
+        $('button.rocksObstacle').click(function(){
+            $('#obstacles').removeClass();
+            $('#obstacles').addClass('rocksObstacle');
+        });
+        $('button.barrelObstacle').click(function(){
+            $('#obstacles').removeClass();
+            $('#obstacles').addClass('barrelObstacle');
+        });
+        $('button.chestObstacle').click(function(){
+            $('#obstacles').removeClass();
+            $('#obstacles').addClass('chestObstacle');
+        });
+    }
+    addObstacles();
 // if #floor has class a specific class, have conditional show/hide on options. 
 // ie. if #floor has a class of .floorWater, show options for water obstacles, like Sharks, debris, boats, pirate enemies etc.
 //have them display=none by default, but when that class is selected, have em display=block
 });
 //Sorry for the soaking wet code here//
+//Also definitely plan on expanding this, as I'm kinda having a lot of fun working on this//
